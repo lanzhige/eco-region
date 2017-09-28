0ad63eca19dfbb82367f95b708569cb2a3f6c591
L.Z <lzhan253@asu.edu>
2017-09-28 10:13:23 -0700

    check branch config

--- preprocess/src/protectedarea.h
+++ preprocess/src/protectedarea.h
@@ -38,25 +38,25 @@ struct ProtectedArea{
     if (polygons.size() == 0) return 0;
     for (unsigned i = 0; i < polygons.size(); i++){
       polygons[i]->resizeBoundingBox();
-<<<<<<< HEAD
-    }
-    Point_2d origin(*polygons[0]->coords[0]), destination(*polygons[0]->coords[0]);
-    for (unsigned i=0; i<polygons.size(); i++){
-      //find the bounding box of entire protected area.
-    }
-   
-      //calculate the bounding box of all the polygons in a protected area
-=======
-    }
+   }
     
     Point_2d origin(*polygons[0]->coords[0]);
     Point_2d destination(*polygons[0]->coords[0]);
 
     for (unsigned i = 0; i < polygons.size(); i++){
-      if (polygons[i]->bbox.origin)
+     //calculate the bounding box of all the polygons in a protected area
+     if (polygons[i]->bbox.origin->x<origin.x) 
+       origin.x = polygons[i]->bbox.origin->x;
+     if (polygons[i]->bbox.origin->y<origin.y)
+       origin.y = polygons[i]->bbox.origin->y;
+     if (polygons[i]->bbox.destination->x>destination.x)
+       destination.x = polygons[i]->bbox.destination->x;
+     if (polygons[i]->bbox.destination->y>destination.y)
+       destination.y = polygons[i]->bbox.destination->y;
     }
-    //calculate the bounding box of all the polygons in a protected area
->>>>>>> 5f7232b804a017efbb864f0cf7050d5f9de161ca
+
+    //calculate cells and output
+
   }
 
   int output(){
